78c65763f909ccddce1d43ff43ea22d314086f34
Eli Holmes <eeholmes@uw.edu>
2021-03-26 10:33:10 -0700

    working on talk slides

--- docs/data/create-mon-sst..R
+++
@@ -1,7 +0,0 @@
-file <- 'data/hawaii_soest_d124_2bb9_c935-5-27-48-80-1979-01-01-2020-06-01.csv'
-aspect_ratio <- c(89,129)
-lat_range <- c(7,13)
-long_range <- c(72,78)
-mon.sst <- processCSV(file=file, aspect_ratio=aspect_ratio, 
-                      lat_range=lat_range,
-                      long_range=long_range)$dat
