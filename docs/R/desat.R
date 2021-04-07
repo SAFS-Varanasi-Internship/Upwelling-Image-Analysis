2d478f686b934df8fa487bef7413b37721d3cbad
Eli Holmes <eeholmes@uw.edu>
2021-04-07 15:09:09 -0700

    Quan Seminar done

--- docs/R/desat.R
+++
@@ -1,8 +0,0 @@
-## Function for desaturating colors by specified proportion
-## https://stackoverflow.com/questions/26314701/r-reducing-colour-saturation-of-a-colour-palette
-desat <- function(cols, sat=0.5) {
-  #library(colorspace)   ## hsv colorspace manipulations
-  #library(RColorBrewer) ## For some example colors
-  X <- diag(c(1, sat, 1)) %*% rgb2hsv(col2rgb(cols))
-  hsv(X[1,], X[2,], X[3,])
-}
--- docs/data/create-mon-sst..R
+++
@@ -1,9 +0,0 @@
-# URL 1979-2020 http://apdrc.soest.hawaii.edu/erddap/griddap/hawaii_soest_d124_2bb9_c935.htmlTable?sst[(2021-02-01T00:00:00Z):1:(2021-02-01T00:00:00Z)][(7):1:(13)][(72):1:(78)]
-# URL 1950-1978 http://apdrc.soest.hawaii.edu/erddap/griddap/hawaii_soest_26fe_f2c9_ed3b.htmlTable?sst[(1950-01-01T00:00:00Z):1:(1978-12-01T00:00:00Z)][(7):1:(13)][(72):1:(78)]
-file <- 'data/hawaii_soest_d124_2bb9_c935-5-27-48-80-1979-01-01-2020-06-01.csv'
-aspect_ratio <- c(89,129)
-lat_range <- c(7,13)
-long_range <- c(72,78)
-mon.sst <- processCSV(file=file, aspect_ratio=aspect_ratio, 
-                      lat_range=lat_range,
-                      long_range=long_range)$dat
